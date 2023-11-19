89f7ae5b5674ec1ec90304c161ee6d12c0e7d73e
klimjs <klimsemenov86@gmail.com>
2023-11-19 15:04:02 +0100

    screen create and delete

--- src/screens/screens.controller.ts
+++ src/screens/screens.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ScreensService } from './screens.service';
+import { Screen } from '@prisma/client';
 
 @Controller('screens')
 export class ScreensController {
   constructor(private screensService: ScreensService) {}
 
   @Get()
-  findAll() {
+  findAll(): Promise<Screen[]> {
     return this.screensService.screens();
+  }
+
+  @Post()
+  create(@Body() data: {}): Promise<Screen> {
+    return this.screensService.create(data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<Screen> {
+    return this.screensService.delete({ id: Number(id) });
   }
 }
--- src/screens/screens.service.ts
+++ src/screens/screens.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { Screen } from '@prisma/client';
+import { Prisma, Screen } from '@prisma/client';
 
 @Injectable()
 export class ScreensService {
@@ -8,5 +8,13 @@ export class ScreensService {
 
   async screens(): Promise<Screen[]> {
     return this.prisma.screen.findMany({ orderBy: { id: 'asc' } });
+  }
+
+  async create(data: Prisma.ScreenCreateInput): Promise<Screen> {
+    return this.prisma.screen.create({ data });
+  }
+
+  async delete(where: Prisma.ScreenWhereUniqueInput): Promise<Screen> {
+    return this.prisma.screen.delete({ where });
   }
 }
